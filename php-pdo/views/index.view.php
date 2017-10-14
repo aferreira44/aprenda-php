277706c49b102b7b7ae9f5217cb6c6f762dd850e
André Ferreira <contato@andreferreira.me>
2017-10-14 15:03:44 -0300

    Views and Partials

--- php-pdo/views/index.view.php
+++ php-pdo/views/index.view.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Classes 101</title>
-</head>
-<body>
-    <a href="/php-pdo/about">About Us</a>
-    <a href="/php-pdo/about/culture">About Our Culture</a>
-    <a href="/php-pdo/contact">Contact</a>
+<?php require 'partials/head.php' ?>
+
+    <h1>My Tasks</h1>
+    
     <ul>
         <?php foreach ($tasks as $task) : ?>
             <li>
@@ -21,5 +13,5 @@
             </li>
         <?php endforeach; ?>
     </ul>
-</body>
-</html>
+
+    <?php require 'partials/footer.php' ?>
---
+++ php-pdo/views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+    <li><a href="/php-pdo/">Home</a></li>
+    <li><a href="/php-pdo/about">About Us</a></li>
+    <li><a href="/php-pdo/about/culture">About Our Culture</a></li>
+    <li><a href="/php-pdo/contact">Contact</a></li>
+    </ul>
+</nav>
