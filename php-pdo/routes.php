eceee0aeaf34ab317290b5a56eb226f2f4367c05
André Ferreira <contato@andreferreira.me>
2017-10-14 22:34:13 -0300

    Forms and Request Types

--- php-pdo/routes.php
+++ php-pdo/routes.php
@@ -1,8 +1,15 @@
 <?php
 
-$router->define([
-    'php-pdo' => 'controllers/index.php',
-    'php-pdo/about' => 'controllers/about.php',
-    'php-pdo/about/culture' => 'controllers/about-culture.php',
-    'php-pdo/contact' => 'controllers/contact.php',
-]);
+// GET
+
+$router->get('php-pdo', 'controllers/index.php');
+
+$router->get('php-pdo/about', 'controllers/about.php');
+
+$router->get('php-pdo/about/culture', 'controllers/about-culture.php');
+
+// POST
+
+$router->post('php-pdo/contact', 'controllers/contact.php');
+
+$router->post('php-pdo/names', 'controllers/add-name.php');
