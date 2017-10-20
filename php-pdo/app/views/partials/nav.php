e1ddacf85f8f3ff51c867ea960fae04ace209d28
André Ferreira <contato@andreferreira.me>
2017-10-20 20:33:47 -0200

    Switch to Namespaces

--- php-pdo/app/views/partials/nav.php
+++ php-pdo/app/views/partials/nav.php
@@ -2,7 +2,7 @@
     <ul>
     <li><a href="/php-pdo/">Home</a></li>
     <li><a href="/php-pdo/about">About Us</a></li>
-    <li><a href="/php-pdo/about/culture">About Our Culture</a></li>
+    <li><a href="/php-pdo/users">Users</a></li>
     <li><a href="/php-pdo/contact">Contact</a></li>
     </ul>
 </nav>
